52696f5455434fa664146fd2ffe7c7a6fb005147
AhuraMagda <lubaszka.magda@gmail.com>
2023-06-04 21:16:26 +0200

    card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,19 @@
 import React from "react";
-import cardPhoto from "../images/firstcard.png"
+import cardPhoto from "../images/firstcard.png";
+import starIcon from "../images/star.png";
 
 function Card() {
     return (
-        <div className="experience-card">
-            <img src={cardPhoto} />
-            <div>X 5.0 (8) USA</div>
+        <div className="card">
+            <img className="card--photo" src={cardPhoto} />
+            <div className="card--stats">
+                <img className="card--star-icon" src={starIcon} />
+                <span>5.0</span>
+                <span className="grey-text">(8)</span>
+                <span className="grey-text">• USA</span>
+            </div>
             <p>Life lessons with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p><b>From $136</b> / person</p>
         </div>
     )
 }
