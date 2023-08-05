63e168bb42fbd170b17aa1693e079b817b9fd501
AhuraMagda <lubaszka.magda@gmail.com>
2023-08-05 15:01:24 +0200

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,9 @@
 import React from "react";
-import mainImage from "../images/Group77.png"
 
 function Main() {
     return (
         <section className="main-section">
-            <img className="main-section__img" src={mainImage} />
+            <img className="main-section__img" src="../images/Group77.png" />
             <h1>Online Experience</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
         </section>
